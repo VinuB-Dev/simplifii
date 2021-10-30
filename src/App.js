0fc5cd746244321619960d308e7ac794d8da7708
vignesh1999 <vinu6th@gmail.com>
2021-10-30 18:31:43 +0530

    frontend done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:3000/scans').then((res) => {
+      setData(res.data)
+    })
+  }, [])
+  const [imageNo, setImageNo] = useState(0)
+  const [click, setClick] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div
+        style={{
+          position: 'absolute',
+          textAlign: 'center',
+          top: '0%',
+          left: '40%',
+          zIndex: '9999',
+        }}
+      >
+        <button className='btn' onClick={() => setImageNo(0)}>
+          First
+        </button>
+        [{imageNo} of {data.length - 1}]
+        <button className='btn' onClick={() => setImageNo(data.length - 1)}>
+          Last
+        </button>
+      </div>
+      <input type='checkbox' id='zoomCheck' />
+      <div className={click === true ? 'carousel-zoomed' : 'carousel'}>
+        <div
+          className='image-container'
+          id='imgs'
+          onClick={() => setClick(!click)}
         >
-          Learn React
-        </a>
-      </header>
+          <label for='zoomCheck'>
+            <img
+              src={data[imageNo]?.url}
+              className={click === true ? 'zoomed' : 'zoom'}
+            />
+          </label>
+        </div>
+        <div class='buttons-container'>
+          <button
+            id='left'
+            class='btn'
+            onClick={() =>
+              setImageNo((imageNo) => (imageNo === 0 ? imageNo : imageNo - 1))
+            }
+          >
+            Prev
+          </button>
+
+          {data.length - 1 === imageNo ? (
+            <button
+              className='btn-complete'
+              id='right'
+              onClick={() => console.log('Reject')}
+            >
+              Complete
+            </button>
+          ) : (
+            <button
+              id='right'
+              class='btn'
+              onClick={() =>
+                setImageNo((imageNo) =>
+                  imageNo === data.length - 1 ? imageNo : imageNo + 1
+                )
+              }
+            >
+              Next
+            </button>
+          )}
+        </div>
+        <div
+          style={{
+            position: 'absolute',
+            textAlign: 'center',
+            bottom: '0%',
+            left: '40%',
+            zIndex: '9999',
+          }}
+        >
+          {data[imageNo]?.approved !== null ? (
+            data[imageNo]?.approved === '0' ? (
+              <button className='btn' onClick={() => console.log('Accept')}>
+                {console.log(data[imageNo]?.approved)}
+                Accept
+              </button>
+            ) : (
+              <button className='btn' onClick={() => console.log('Reject')}>
+                {console.log(data[imageNo]?.approved)}
+                Reject
+              </button>
+            )
+          ) : (
+            <div>
+              <button className='btn' onClick={() => console.log('Accept')}>
+                Accept
+              </button>
+              <button className='btn' onClick={() => console.log('Reject')}>
+                Reject
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
